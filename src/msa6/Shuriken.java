1bfccd8745ce9911becd9db37d1ba67e171366de
Carl Levan <cllevan123@gmail.com>
2019-01-09 08:23:44 -0500

    Added ninja monkey and ninja shuriken

---
+++ src/msa6/Shuriken.java
@@ -0,0 +1,25 @@
+package msa6;
+
+import java.util.LinkedList;
+
+public class Shuriken extends Projectile{
+	
+	/**
+	 * constructor
+	 * @param po the position of the projectile
+	 * @param pa the path of the projectile
+	 * @param rb the rbe pop of the projectile
+	 * @param d the durability of the projectile
+	 * @param ra the radius (size) of the projectile
+	 */
+
+	public Shuriken(Coordinate po, LinkedList<Coordinate> pa, int rb, int d, double ra) {
+		super(po, pa, 400, rb, d, ra);
+	}
+
+	@Override
+	public ProjectilePopOutput pop() {
+		super.durability --;
+		return new ProjectilePopOutput(new LinkedList<Effect>(), new LinkedList<Projectile>());
+	}
+}
